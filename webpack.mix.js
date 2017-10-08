658b601d3b2d29537a5ea121ad715413acb80c6c
webmarkelov <webmarkelov@gmail.com>
2017-10-08 22:21:05 +0300

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,11 @@
+const mix = require('laravel-mix');
+
+mix
+  .sass('app/assets/scss/app.scss', 'app/dist/')
+  .options({
+    postCss: [
+      require('postcss-cssnext')(),
+      require('postcss-short')(),
+      require('postcss-assets')(),
+    ],
+  });
